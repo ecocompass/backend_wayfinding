0e19e2c591e36c20a00d350f91c1ab753cff24db
Anupal Mishra <anupal.pm@gmail.com>
2024-01-31 22:04:13 +0000

    JSON based responses and POJO mappings for Nodes & Edges.

---
+++ src/main/java/org/ecocompass/core/overpass/OverpassEdge.java
@@ -0,0 +1,41 @@
+package org.ecocompass.core.overpass;
+
+import java.util.HashSet;
+
+public class OverpassEdge {
+    private final HashSet<String> allowedTypes = new HashSet<String>() {{
+        add("dart");
+        add("luas");
+        add("road");
+        add("walk");
+        add("cycle");
+    }};
+    final String type;
+    public OverpassNode start;
+    public OverpassNode end;
+
+    public double geoDistance;
+
+    public OverpassEdge(OverpassNode start, OverpassNode end, String type) {
+        this.start = start;
+        this.end = end;
+
+        if (!allowedTypes.contains(type)) {
+            throw new RuntimeException("Invalid Edge type '" + type + "'");
+        } else {
+            this.type = type;
+        }
+
+        // calculateDistance(); // TODO: fix - function logic fails currently.
+    }
+
+    private void calculateDistance() {
+        double latDistance = Math.toRadians(end.latitude - start.latitude);
+        double lonDistance = Math.toRadians(end.longitude - start.longitude);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(start.latitude)) * Math.cos(Math.toRadians(end.latitude))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        geoDistance = 6371 * c;
+    }
+}
