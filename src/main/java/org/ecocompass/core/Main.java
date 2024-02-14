dc03469cf248dd371a2f321343128a1413e2a22e
Rahul Singh <rahulsingh14jan95@gmail.com>
2024-02-14 18:02:23 +0530

    KDTree Implementation

--- src/main/java/org/ecocompass/core/Main.java
+++ src/main/java/org/ecocompass/core/Main.java
@@ -1,5 +1,6 @@
 package org.ecocompass.core;
 
+import org.ecocompass.core.K_DTree.KDTree;
 import org.ecocompass.core.graph.Graph;
 import org.ecocompass.core.graph.Node;
 import org.ecocompass.core.overpass.Overpass;
@@ -12,11 +13,15 @@ public class Main {
         System.out.println("Ecocompass Core Engine");
 
         Overpass overpass = new Overpass();
-//         String response = overpass.queryLocation("IE", "Dublin", "Rathmines");
-//         overpass.saveQueryOutput(response, "query_data.json");
-        String response = overpass.loadSavedQueryOutput("query_data.json");
+        String response = overpass.queryLocation("IE", "Dublin", "Rathmines");
+        overpass.saveQueryOutput(response, "query_data.json");
+        //String response = overpass.loadSavedQueryOutput("query_data.json");
 
         Graph graph =  overpass.createGraphFromOverpassQuery(response);
+
+        KDTree tree = overpass.createTreeFromGraph(graph);
+        //double[] point = {101234, 12235};
+        //tree.findNearestNeighbor(point);
 
         // grosvenor square to trinity
         List<Node> route = graph.shortestPath(59802249L, 2414635995L, "road");
---
+++ src/test/java/api/controller/DesignA2BTest.java
@@ -0,0 +1,114 @@
+package api.controller;
+
+import org.ecocompass.core.graph.Node;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DesignA2BTest {
+
+    private PathFinder pathFinder;
+
+    @BeforeEach
+    public void setUp() {
+        pathFinder = mock(PathFinder.class); // Mock the PathFinder class
+    }
+
+    @Test
+    public void testFindShortestPathWithPreferences() {
+        //Test for only personal vehicle
+        UserPreferences userPreferences = new UserPreferences(true, false, false);
+
+        double latitude = 40.7128;
+        double longitude = -74.0060;
+        double lat = 34.0522;
+        double longs = -118.2437;
+
+        Node node1 = new Node(latitude, longitude);
+        Node node2 = new Node(lat,longs);
+
+        when(pathFinder.findShortestPath(node1, node2, any(UserPreferences.class)))
+                .thenReturn("");
+
+        verify(pathFinder).findShortestPath(node1, node2, eq(userPreferences));
+
+        ReturnedPaths result = pathFinder.findShortestPath(node1, node2, userPreferences);
+
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testFindShortestPathWithPreferences() {
+        // Test for only cycle or walking
+        UserPreferences userPreferences = new UserPreferences(false, true, false);
+
+        double latitude = 40.7128;
+        double longitude = -74.0060;
+        double lat = 34.0522;
+        double longs = -118.2437;
+
+        Node node1 = new Node(latitude, longitude);
+        Node node2 = new Node(lat,longs);
+
+        when(pathFinder.findShortestPath(node1, node2, any(UserPreferences.class)))
+                .thenReturn(new ArrayList<>());
+
+        verify(pathFinder).findShortestPath(node1, node2, eq(userPreferences));
+
+        ReturnedPaths result = pathFinder.findShortestPath(node1, node2, userPreferences);
+
+        assertEquals(new ArrayList<>(), result);
+    }
+
+
+    @Test
+    public void testFindShortestPathWithPreferences() {
+        //Test for only public transport
+        UserPreferences userPreferences = new UserPreferences(false, false, true);
+
+        double latitude = 40.7128;
+        double longitude = -74.0060;
+        double lat = 34.0522;
+        double longs = -118.2437;
+
+        Node node1 = new Node(latitude, longitude);
+        Node node2 = new Node(lat,longs);
+
+        when(pathFinder.findShortestPath(node1, node2, any(UserPreferences.class)))
+                .thenReturn(new ArrayList<>());
+
+        verify(pathFinder).findShortestPath(node1, node2, eq(userPreferences));
+
+        ReturnedPaths result = pathFinder.findShortestPath(node1, node2, userPreferences);
+
+        assertEquals(new ArrayList<>(), result);
+    }
+
+    @Test
+    public void testFindShortestPathWithPreferences() {
+        //Test for only personal vehicle
+        UserPreferences userPreferences = new UserPreferences(true, false);
+
+        double latitude = 40.7128;
+        double longitude = -74.0060;
+        double lat = 34.0522;
+        double longs = -118.2437;
+
+        Node node1 = new Node(latitude, longitude);
+        Node node2 = new Node(lat,longs);
+
+        when(pathFinder.findShortestPathReroute(node1, node2, any(UserPreferences.class)))
+                .thenReturn(new ArrayList<>());
+
+        verify(pathFinder).findShortestPathReroute(node1, node2, eq(userPreferences));
+
+        ReturnedPaths result = pathFinder.findShortestPathReroute(node1, node2, userPreferences);
+
+        assertEquals(new ArrayList<>(), result);
+    }
+
+}
