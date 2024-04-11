5f65a1d01e1e24c2e68f95303773bdec3ee1162f
Rahul Singh <rahulsingh14jan95@gmail.com>
2024-04-11 16:16:07 +0530

    Optimisation

---
+++ src/main/java/org/ecocompass/core/util/KdNodeCache.java
@@ -0,0 +1,21 @@
+package org.ecocompass.core.util;
+
+import org.ecocompass.core.K_DTree.KdNode;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class KdNodeCache {
+    private final Map<String, KdNode> cache = new HashMap<>();
+
+    public KdNode get(String nodeId) {
+        return cache.get(nodeId);
+    }
+
+    public void put(String nodeId, KdNode node) {
+        cache.put(nodeId, node);
+    }
+}
